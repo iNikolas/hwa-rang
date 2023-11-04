0c4e693c7c3b9d538c67da73d306b04110b40663
Mykola Lebid <realkolos1@gmail.com>
2023-11-04 07:43:34 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 import path from "path";
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg}"],
+      },
+      manifest: {
+        name: "Тхеквондо Київ [HWA-RANG]",
+        short_name: "Тхеквондо",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icons/favicon-package/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/favicon-package/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       theme: path.resolve(__dirname, "./src/theme"),
