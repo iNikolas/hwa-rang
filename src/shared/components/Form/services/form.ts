e3427d04a8f13161596955a8f4c0bfc2712dec2b
Mykola Lebid <realkolos1@gmail.com>
2023-11-08 07:13:47 +0200

    server settings validation to send the form

--- src/shared/components/Form/services/form.ts
+++ src/shared/components/Form/services/form.ts
@@ -2,6 +2,13 @@ import { FormSchema } from "../types";
 import { mapKeyToName } from "../utils";
 
 export const sendForm = async (rawData: FormSchema): Promise<void> => {
+  const action = import.meta.env.VITE_FORM_ACTION;
+  const contentType = import.meta.env.VITE_FORM_CONTENT_TYPE;
+
+  if (typeof action !== "string" || typeof contentType !== "string") {
+    throw new Error("Missing required Settings on the Server to sent the Form");
+  }
+
   const formData = new URLSearchParams();
 
   Object.entries(rawData).forEach(([key, value]) => {
@@ -13,10 +20,10 @@ export const sendForm = async (rawData: FormSchema): Promise<void> => {
     formData.append(name, value);
   });
 
-  const response = await fetch(import.meta.env.VITE_FORM_ACTION, {
+  const response = await fetch(action, {
     method: "POST",
     body: formData,
-    headers: { "Content-Type": import.meta.env.VITE_FORM_CONTENT_TYPE },
+    headers: { "Content-Type": contentType },
   });
 
   if (!response.ok) {
